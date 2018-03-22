9183d3a89977017010e1490cd7d86d638131bfbf
liuyutaoLYT <734034594@qq.com>
2018-03-22 11:07:02 +0800

    add

---
+++ code/ChuSeProject/src/com/chuse/entity/User.java
@@ -0,0 +1,45 @@
+package com.chuse.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User {
+	private int id;
+	private String name;
+	private String email;
+	private String password;
+	private String picture;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPicture() {
+		return picture;
+	}
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+	
+	
+	
+}
